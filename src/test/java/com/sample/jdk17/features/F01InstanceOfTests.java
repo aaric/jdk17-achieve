b43bd42d3fb92b51ad62d59d7dc2dd06f678ccd4
qlhan <qlhan@incarcloud.com>
2021-10-25 14:50:16 +0800

    0.2.0-SNAPSHOT f01-instanceof

--- src/test/java/com/sample/jdk17/features/F01InstanceOfTests.java
+++ src/test/java/com/sample/jdk17/features/F01InstanceOfTests.java
@@ -1,4 +1,4 @@
-package com.sample.jdk17.ch01;
+package com.sample.jdk17.features;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
---
+++ src/test/java/com/sample/jdk17/features/F02SwitchTests.java
@@ -0,0 +1,22 @@
+package com.sample.jdk17.features;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * F02: Switch expressions
+ *
+ * @author Aaric, created on 2021-10-20T17:55.
+ * @version 0.2.0-SNAPSHOT
+ */
+public class F02SwitchTests {
+
+    @Test
+    public void testOrigin() {
+
+    }
+
+    @Test
+    public void testNow() {
+
+    }
+}
