02cde61dba32f2d197ba39245ceffb9172c13f90
qlhan <qlhan@incarcloud.com>
2022-05-23 17:40:50 +0800

    0.2.0-SNAPSHOT module-info.java

---
+++ src/test/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * jdk17.achieve.test
+ *
+ * @author Aaric, created on 2022-05-23T15:09.
+ * @version 0.1.0-SNAPSHOT
+ */
+module jdk17.achieve.test {
+    requires lombok;
+    requires org.junit.jupiter.api;
+    requires commons.beanutils;
+}
