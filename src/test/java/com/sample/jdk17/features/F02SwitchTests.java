b8e06770202ecea6b06fcc7614e334d188bcb120
qlhan <qlhan@incarcloud.com>
2022-10-18 11:40:56 +0800

    0.2.0-SNAPSHOT f01-switch

--- src/test/java/com/sample/jdk17/features/F02SwitchTests.java
+++ src/test/java/com/sample/jdk17/features/F02SwitchTests.java
@@ -1,6 +1,10 @@
 package com.sample.jdk17.features;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * F02: Switch expressions
@@ -10,13 +14,35 @@ import org.junit.jupiter.api.Test;
  */
 public class F02SwitchTests {
 
+    private static final String KEY_ID = "ID";
+    private static final String KEY_NAME = "NAME";
+
+    private static Map<String, Object> dataMap;
+
+    @BeforeAll
+    public static void setUp() {
+        dataMap = new HashMap<>();
+        dataMap.put(KEY_ID, 1);
+        dataMap.put(KEY_NAME, "Aaric");
+    }
+
     @Test
     public void testOrigin() {
-        // TODO
+        Map<String, Object> dataMap = new HashMap<>();
+
+        if(dataMap.get(KEY_ID) instanceof Integer id){
+            System.err.println(id);
+        } else if (dataMap.get(KEY_NAME) instanceof String name) {
+            System.err.println(name);
+        }
     }
 
     @Test
     public void testNow() {
-        // TODO
+        switch (dataMap.get(KEY_ID)) {
+            case Integer i -> System.err.println(i);
+            case Double d -> System.err.println(d);
+            default -> System.err.println();
+        }
     }
 }
